adba2603d7fcd8cdfbf27cb37501ca09ced8ad78
Daniel Peralles <peralles@gmail.com>
2022-01-13 11:53:30 -0300

    alterado lógica

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ var fs = require("fs");
 function convertExcelFileToJsonUsingXlsx(filePath) {
   // Read the file using pathname
   const file = xlsx.readFile(filePath);
+  readFileXlsxConvert(file);
+}
 
+function readFileXlsxConvert(xlsxFile) {
   // Grab the sheet info from the file
-  const sheetNames = file.SheetNames;
+  const sheetNames = xlsxFile.SheetNames;
   const totalSheets = sheetNames.length;
 
   // Loop through sheets
   for (let i = 0; i < totalSheets; i++) {
     // Convert to json using xlsx
-    const tempData = xlsx.utils.sheet_to_json(file.Sheets[sheetNames[i]]);
+    const tempData = xlsx.utils.sheet_to_json(xlsxFile.Sheets[sheetNames[i]]);
     // Skip header row which is the colum names
     tempData.shift();
 
